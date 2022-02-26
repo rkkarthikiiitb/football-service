b718dc5ad2bd83d578595ad704662ba9c55aba7b
karthikk <getkarthikin@gmail.com>
2022-02-26 19:57:49 +1100

    contract-relu: initial commit

---
+++ src/main/java/com/league/football/service/FootballServiceApplication.java
@@ -0,0 +1,13 @@
+package com.league.football.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/league/football/service/FootballServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.league.football.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
